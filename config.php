0154380b5494d24746313c322e0047be20dbc298
Alex Korolev <alex.korolev.1995@gmail.com>
2019-09-23 19:58:59 +0300

    Add config.php: DataBase config

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+define('DB', array(
+                    'type' => 'mysql',
+                    'user' => 'root',
+                    'pass' => 'get5',
+                    'host' => 'localhost',
+                    'dbname' => 'task',
+                    'charset' => 'utf8',
+                ));
