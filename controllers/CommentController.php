a2222abc69e6c444956cb49e8476c30621a3f576
Alex Korolev <alex.korolev.1995@gmail.com>
2019-09-25 22:19:26 +0300

    СommentModel и CommentController отрефакторины

--- controllers/CommentController.php
+++ controllers/CommentController.php
@@ -3,13 +3,17 @@
 class CommentController
 {
     public static function newAction($taskId) {
-        if($_SESSION['auth'] !== null) {
-            CommentModel::addComment($taskId);
-            header("Location: /task/show/$taskId");
-            exit();
-        } else {
-            $view = new NormalView('Not permissions');
-            $view->addElement('error', 'Not permissions')->render();
+        try {
+            if($_SESSION['auth'] !== null) {
+                CommentModel::addComment($taskId);
+                header("Location: /task/show/$taskId");
+                exit();
+            } else {
+                throw new Exception("Not permissions", 1);
+            }
+        } catch (Exception $e) {
+            $view = new NormalView('Error');
+            $view->addElement('error', $e->getMessage())->render();
         }
     }
 }
--- models/CommentModel.php
+++ models/CommentModel.php
@@ -3,15 +3,23 @@
 class CommentModel
 {
     public static function getComments($taskId) {
-        $comments = DB::run("SELECT * FROM comments WHERE task=?", $taskId)->fetchAll();
-        $comments = array_reverse($comments);
-        foreach ($comments as $comment) {
-            yield $comment;
+        try {
+            $comments = DB::run("SELECT * FROM comments WHERE task=?", $taskId)->fetchAll();
+            $comments = array_reverse($comments);
+            foreach ($comments as $comment) {
+                yield $comment;
+            }
+        } catch (PDOException $e) {
+            throw new Exception("Failed DB request");
         }
     }
 
     public static function addComment($taskId) {
-        DB::run("INSERT INTO comments SET comment=?, author=?, task=?",
+        try {
+            DB::run("INSERT INTO comments SET comment=?, author=?, task=?",
                             $_POST['comment'], $_SESSION['auth'], $taskId);
+        } catch (PDOException $e) {
+            throw new Exception("Failed to add new comment");
+        }
     }
 }
