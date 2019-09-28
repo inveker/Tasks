e02d14a1c2a6ad4e8bb65c46f1e53ad123d2ba82
Alex Korolev <alex.korolev.1995@gmail.com>
2019-09-28 16:00:23 +0300

    Функция rand() заменена на mt_rand(), каптча теперь генерирует случайные цвета

--- core/captcha.php
+++ core/captcha.php
@@ -2,12 +2,12 @@
 
 session_start();
 
-$captcha = rand(1000, 9999);
+$captcha = mt_rand(1000, 9999);
 $_SESSION['captcha'] = $captcha;
 
 $im = imagecreatetruecolor(100, 50);
 
-$white = imagecolorallocate($im, 255, 255, 255);
+
 $grey = imagecolorallocate($im, 100, 100, 100);
 $background = imagecolorallocate($im, 255, 222, 173);
 
@@ -16,12 +16,13 @@ imagefilledrectangle($im, 0, 0, 100, 50, $background);
 $font = '../fonts/captcha_font.ttf';
 
 $arr = str_split($captcha);
-for($i=0; $i < 4; $i++) { 
-    $angle = rand(-15, 15);
-    $size = rand(24, 32);
-    $height = rand(25, 45);
+for($i=0; $i < 4; $i++) {
+    $angle = mt_rand(-15, 15);
+    $size = mt_rand(24, 32);
+    $height = mt_rand(25, 45);
+    $color = imagecolorallocate($im, mt_rand(100, 255), mt_rand(100, 255), mt_rand(100, 255));
     imagettftext($im, $size, $angle, 15+20*$i, $height, $grey, $font, $arr[$i]);
-    imagettftext($im, $size, $angle, 17+20*$i, $height+2, $white, $font, $arr[$i]);
+    imagettftext($im, $size, $angle, 17+20*$i, $height+2, $color, $font, $arr[$i]);
 }
 
 header ("Content-type: image/png");
