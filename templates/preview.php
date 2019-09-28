02dbb392891b72eb3ea93e61c6b9856f7f12091e
Alex Korolev <alex.korolev.1995@gmail.com>
2019-09-28 16:06:59 +0300

    добавлен пробел в шаблон

--- templates/preview.php
+++ templates/preview.php
@@ -1,7 +1,7 @@
 <a class="preview" href="/task/show/<?=$id?>">
     <div>
         <b>ID#<?=$id?></b><span class="date"><?=$date?></span>
-        <p><b>Description:</b><?=$description?></p>
+        <p><b>Description: </b><?=$description?></p>
         <b>Author: </b><i><?=$author?></i>
     </div>
 </a>
